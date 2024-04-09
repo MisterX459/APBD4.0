972d70594a627441a4ce4d3ce3832e36b2b150c8
MisterX459 <65484118+MisterX459@users.noreply.github.com>
2024-04-09 11:44:56 +0200

    Added small changes

---
+++ APBD4.0/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD4._0;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string Category { get; set; }
+    public double Weight { get; set; }
+    public string FurColor { get; set; }
+}
