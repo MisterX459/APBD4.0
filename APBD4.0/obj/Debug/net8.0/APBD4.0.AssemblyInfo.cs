b354fac6a5ec4d86909dc5abd6f48ced00799765
MisterX459 <65484118+MisterX459@users.noreply.github.com>
2024-04-15 20:27:42 +0200

    Final stage of the project

--- APBD4.0/obj/Debug/net8.0/APBD4.0.AssemblyInfo.cs
+++ APBD4.0/obj/Debug/net8.0/APBD4.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD4.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972d70594a627441a4ce4d3ce3832e36b2b150c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD4.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD4.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
